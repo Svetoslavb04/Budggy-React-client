f13fe11495e5b1417faea19b32a555095a3ee59b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-09 22:04:21 +0200

    feat: install sass

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
+import './index.scss';
 
 import App from './Components/App/App';
 
