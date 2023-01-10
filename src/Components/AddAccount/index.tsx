5f451e10720d6ffebc6d02ecbf1cd6f3a546a735
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-10 23:38:53 +0200

    feat: add AddAccount Component

---
+++ src/Components/AddAccount/index.tsx
@@ -0,0 +1,55 @@
+import './AddAccount.scss';
+
+import { FC } from "react"
+
+import SmallWidget from '../Core/SmallWidget';
+
+import { BsBank2, BsCashCoin } from 'react-icons/bs'
+
+const AddAccount: FC = () => {
+
+    const handleAddBank = () => {
+
+    }
+
+    const handleAddManualAccount = () => {
+        
+    }
+
+    return (
+        <div
+            className="add-account-container widget"
+        >
+            <h2>Add account</h2>
+            <div className='divider'></div>
+            <div className='account-options'>
+                <SmallWidget
+                    className='account-option'
+                    onClick={handleAddBank}
+                >
+                    <div className='account-option-info'>
+                        <h3>Bank Sync</h3>
+                        <p>Synchronize your bank account transactions automatically</p>
+                    </div>
+                    <div className='account-option-icon'>
+                        <BsBank2 />
+                    </div>
+                </SmallWidget>
+                <SmallWidget
+                    className='account-option'
+                    onClick={handleAddManualAccount}
+                >
+                    <div className='account-option-info'>
+                        <h3>Manual</h3>
+                        <p>Create an account which you will update manually.</p>
+                    </div>
+                    <div className="account-option-icon">
+                        <BsCashCoin />
+                    </div>
+                </SmallWidget>
+            </div>
+        </div>
+    )
+}
+
+export default AddAccount
--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,14 +1,18 @@
 import './App.scss';
 
-import AccountCard from '../AccountCard';
 import { useState } from 'react';
 
 import { BsPlusLg } from 'react-icons/bs'
 
 import AccountCard from '../AccountCard';
 import SmallWidget from '../Core/SmallWidget';
+import Modal from '../Core/Modal';
+import AddAccount from '../AddAccount';
 
 function App() {
+
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
   return (
     <div id='app' className="app">
       <header>
@@ -17,13 +21,17 @@ function App() {
           <AccountCard name='DSK Bank' currency='BGN' balance={1000} />
           <AccountCard name='Revolut' currency='BGN' balance={1000} />
           <AccountCard name='UniCredit' currency='BGN' balance={1000} />
-          <SmallWidget className='add-account'>
+          <SmallWidget className='add-account' onClick={setIsCreateModalOpen.bind(null, true)}>
             <div className='add-account-icon-container'>
               <BsPlusLg />
             </div>
           </SmallWidget>
+          <Modal opened={isCreateModalOpen} close={setIsCreateModalOpen.bind(null, false)}>
+            <div className='add-account-wrapper'>
+              <AddAccount />
+            </div>
+          </Modal>
         </div>
-
       </header>
     </div>
   );
