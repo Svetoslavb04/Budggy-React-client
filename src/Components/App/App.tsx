5033c2099788ab514e4e3b1b9cf2e38185849973
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-10 23:41:25 +0200

    feat: extract content in Content component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,38 +1,12 @@
+import Content from '../Core/Content';
 import './App.scss';
-
-import { useState } from 'react';
-
-import { BsPlusLg } from 'react-icons/bs'
-
-import AccountCard from '../AccountCard';
-import SmallWidget from '../Core/SmallWidget';
-import Modal from '../Core/Modal';
-import AddAccount from '../AddAccount';
 
 function App() {
 
-  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
   return (
     <div id='app' className="app">
-      <header>
-        <div className='accounts-list'>
-          <AccountCard name='Central Cooperative Bank' currency='BGN' balance={1000} />
-          <AccountCard name='DSK Bank' currency='BGN' balance={1000} />
-          <AccountCard name='Revolut' currency='BGN' balance={1000} />
-          <AccountCard name='UniCredit' currency='BGN' balance={1000} />
-          <SmallWidget className='add-account' onClick={setIsCreateModalOpen.bind(null, true)}>
-            <div className='add-account-icon-container'>
-              <BsPlusLg />
-            </div>
-          </SmallWidget>
-          <Modal opened={isCreateModalOpen} close={setIsCreateModalOpen.bind(null, false)}>
-            <div className='add-account-wrapper'>
-              <AddAccount />
-            </div>
-          </Modal>
-        </div>
-      </header>
+      <Content />
     </div>
   );
 }
---
+++ src/Components/Core/Content/index.tsx
@@ -0,0 +1,37 @@
+import './Content.scss';
+
+import { useState } from 'react';
+
+import { BsPlusLg } from 'react-icons/bs'
+
+import AccountCard from '../../AccountCard';
+import SmallWidget from '../../Core/SmallWidget';
+import Modal from '../../Core/Modal';
+import AddAccount from '../../AddAccount';
+
+const Content = () => {
+
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+
+    return (
+        <div className='accounts-list'>
+            <AccountCard name='Central Cooperative Bank' currency='BGN' balance={1000} />
+            <AccountCard name='DSK Bank' currency='BGN' balance={1000} />
+            <AccountCard name='Revolut' currency='BGN' balance={1000} />
+            <AccountCard name='UniCredit' currency='BGN' balance={1000} />
+            <SmallWidget className='add-account' onClick={setIsCreateModalOpen.bind(null, true)}>
+                <div className='add-account-icon-container'>
+                    <BsPlusLg />
+                </div>
+            </SmallWidget>
+            <Modal opened={isCreateModalOpen} close={setIsCreateModalOpen.bind(null, false)}>
+                <div className='add-account-wrapper'>
+                    <AddAccount />
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+export default Content
