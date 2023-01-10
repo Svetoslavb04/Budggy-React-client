608cc523c2ff9dd2f477ac9d551840ceb1ccdffb
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-10 22:55:31 +0200

    feat: add Modal component

---
+++ src/Components/Core/Modal/index.tsx
@@ -0,0 +1,33 @@
+import './Modal.scss';
+
+import { FC, ReactNode, MouseEvent } from "react"
+
+export type Props = {
+    opened: boolean,
+    close: () => void,
+    children: ReactNode
+}
+
+const Modal: FC<Props> = ({ opened, close, children }) => {
+
+    const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+
+        const target = e.target as Element;
+        
+        if (target.classList.contains('modal')) {
+            close()        
+        }
+
+    }
+
+    return (
+        <div
+            className={`modal${opened ? ' modal-visible' : ''}`}
+            onClick={handleClick}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default Modal
