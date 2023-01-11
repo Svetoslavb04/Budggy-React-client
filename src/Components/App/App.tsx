068f62ac275b55ece1d33a2df5b1906206fcf80b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-11 23:19:27 +0200

    feat: install MaterialUI

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,12 +3,32 @@ import './App.scss';
 import AuthProvider from '../../context/authContext';
 
 import Content from '../Content';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        light: '#4CC441',
+        main: '#4E9F3D',
+        dark: '#1E5128'
+      },
+      secondary: {
+        light: '#202120',
+        main: '#191A19',
+        dark: '#0a0a0a'
+      }
+    }
+  });
+
   return (
     <div id='app' className="app">
       <AuthProvider>
-        <Content />
+        <ThemeProvider theme={theme}>
+          <Content />
+        </ThemeProvider>
       </AuthProvider>
     </div>
   );
